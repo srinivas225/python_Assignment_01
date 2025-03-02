0f34d6eac11f57048d9d2b726d3f4ed30a100686
Srinivas chevula <53026836+srinivas225@users.noreply.github.com>
2025-03-03 00:32:30 +0530

    Add files via upload

---
+++ math_operations.py
@@ -0,0 +1,9 @@
+# Perform Basic Mathematical Operations
+# Taking two numbers as input from user
+num1 =int(input("Enter the first number: \n"))
+num2 =int(input("Enter the Second number: \n"))
+
+print("Addition of two number:",num1+num2)
+print("Subtraction of two number:",num1-num2)
+print("Multiplication of two number:",num1*num2)
+print("Division of two number:",num1/num2)

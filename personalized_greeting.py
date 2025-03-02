ce2f0ac6b86810c76cc8af5c2670bfcaaa0ec417
Srinivas chevula <53026836+srinivas225@users.noreply.github.com>
2025-03-03 00:44:58 +0530

    Add files via upload

---
+++ personalized_greeting.py
@@ -0,0 +1,4 @@
+# Taking firts anme and last name as input
+first_name = input('Enter your First name: \n')
+last_name  = input("Enter your last name: \n")
+print(first_name + " " + last_name +"! " + "Welcome to the python program")

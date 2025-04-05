9e25b37a5e186b96cc284b127bac10f4ae8d0464
Srinivas chevula <53026836+srinivas225@users.noreply.github.com>
2025-04-05 17:25:20 +0530

    Add files via upload

---
+++ Math Module for Calculations.py
@@ -0,0 +1,13 @@
+import math
+
+
+number =float(input("Enter number"))
+
+sqrt_root =math.sqrt(number)
+log_result =math.log(number)
+sin_result =math.sin(number)
+
+print (f"Square root: {sqrt_root} ")
+print (f"log_result : {log_result } ")
+print (f"sin_result  {sin_result } ")
+
